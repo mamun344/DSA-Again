f9639e04b6c5172f7b3ec6ead381071cc44c6163
Mamun <abdullah.mamun@bongobd.com>
2025-04-15 18:19:55 +0600

    Array: Max Profit

---
+++ Array MaxProfit/Array MaxProfit/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  Array MaxProfit
+//
+//  Created by Mamun on 15/4/25.
+//
+
+import Foundation
+
+func maxProfit(_ prices: [Int]) -> Int {
+    
+    var profit = 0    
+    var buy = prices[0]
+    
+    for i in 1..<prices.count {
+        if prices[i] > buy {
+            profit = max(profit, prices[i] - buy)
+        }
+        
+        buy = min(buy, prices[i])
+    }
+    
+    return profit
+}
+
+//print(maxProfit([7, 1, 5, 3, 6, 4]))
+print(maxProfit([7,6,4,3,1]))
