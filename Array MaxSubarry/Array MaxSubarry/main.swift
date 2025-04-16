420fec91e152714badcce87121b4d1ef064b0b1d
Mamun <abdullah.mamun@bongobd.com>
2025-04-16 20:50:48 +0600

    Array: max Sub array

---
+++ Array MaxSubarry/Array MaxSubarry/main.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  Array MaxSubarry
+//
+//  Created by Mamun on 16/4/25.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/maximum-subarray
+
+func maxSubArray(_ nums: [Int]) -> Int {
+
+    var sum: Int = nums[0]
+    var maxSum: Int = nums[0]
+    
+    for i in 1..<nums.count {
+        print(nums[i], sum, sum + nums[i])
+        sum = max(nums[i], sum + nums[i]) // not clear 
+        maxSum = max(maxSum, sum)
+        print(maxSum)
+    }
+    
+    return maxSum
+}
+
+print(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+
